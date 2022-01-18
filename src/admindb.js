9ffde919e053ec61a1793f141ead4fba0755d603
rithi <ritimahesh29@gmail.com>
2022-01-18 12:48:44 +0530

    new commit

--- src/admindb.js
+++ src/admindb.js
@@ -1,23 +1,27 @@
-const { connect } = require('./db-connection');
-const mongoose = require('mongoose');
-const url = "mongodb+srv://user:pwd@wedlock-cluster.da3ng.mongodb.net/admindb";
+const { connect } = require("./db-connection");
+const mongoose = require("mongoose");
+// const url = "mongodb+srv://user:pwd@wedlock-cluster.da3ng.mongodb.net/admindb";
+const url =
+  "mongodb+srv://iamrithi:iamrithi333@cluster0.gdtlt.mongodb.net/USER?retryWrites=true&w=majority";
 let db;
-const tenantSchema = new mongoose.Schema({
-    id: Number,
-    name: String
-}, { timestamps: true })
+const tenantSchema = new mongoose.Schema(
+  {
+    tenantName: String,
+  },
+  { timestamps: true }
+);
 
 const tenantModel = mongoose.model("tenants", tenantSchema);
 
 const getDb = async () => {
-    return db ? db : await connect(url)
-}
+  return db ? db : await connect(url);
+};
 
 const getTenantModel = async () => {
-    const adminDb = await getDb();
-    return adminDb.model("tenants", tenantSchema)
-}
+  const adminDb = await getDb();
+  return adminDb.model("tenants", tenantSchema);
+};
 
 module.exports = {
-    getTenantModel
-}
+  getTenantModel,
+};
--- src/index.js
+++ src/index.js
@@ -1,49 +1,55 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
 
 const { getTenantModel } = require("./admindb");
 const { getCustomerModel } = require("./tenantdb");
 
-app.get('/tenant', async (req, res) => {
-    let tenantId = req.query.tenantId;
-    let tenantModel = await getTenantModel();
-    const tenant = new tenantModel({ id: tenantId, name: tenantId });
-    let doc = await tenantModel.findOneAndUpdate({ id: tenantId }, { id: tenantId, name: tenantId });
-    if (!doc) {
-        tenant.save(function (err) {
-            // if (err) return handleError(err);
-            // saved!
-        });
-    }
+// app.use(require("body-parser"));
+app.use(express.json());
+app.post("/tenant", async (req, res) => {
+  var data = {
+    tenantName: req.body.tenantName,
+  };
 
-    res.send(JSON.stringify(tenant))
-})
+  let tenantModel = await getTenantModel();
+  const tenant = new tenantModel({ id: data.tenantName });
+  let doc = await tenantModel.create(data);
+  if (!doc) {
+    tenant.save(function (err) {
+      // if (err) return handleError(err);
+      // saved!
+    });
+  }
+  res.send(JSON.stringify(tenant));
+});
 
-app.get('/customer', async (req, res) => {
-    let tenantId = req.query.tenantId;
-    let customerName = req.query.customer;
-    let tenantModel = await getTenantModel();
-    let tenant = await tenantModel.findOne({ id: tenantId })
-    if (!tenant)
-        res.sendStatus(404) // tenant not found. Register tenant
-    let customerModel = await getCustomerModel(tenantId);
-    const customer = new customerModel({ customerName });
-    let doc = await customerModel.findOneAndUpdate({ customerName }, { customerName });
-    if (!doc) {
-        customer.save(function (err) {
-            // if (err) return handleError(err);
-            // saved!
-        });
-    }
+app.post("/customer", async (req, res) => {
+  var data = {
+    customerName: req.body.customerName,
+    age: req.body.age,
+  };
+  let tenantId = req.query.tenantId;
+  // let customerName = req.query.customer;
+  let tenantModel = await getTenantModel();
+  let tenant = await tenantModel.findOne({ id: tenantId });
+  if (!tenant) res.sendStatus(404); // tenant not found. Register tenant
+  let customerModel = await getCustomerModel(tenantId);
+  const customer = new customerModel(data);
+  let doc = await customerModel.create(customer);
+  if (!doc) {
+    customer.save(function (err) {
+      // if (err) return handleError(err);
+      // saved!
+    });
+  }
 
-    res.send(JSON.stringify(customer))
-})
-
+  res.send(JSON.stringify(customer));
+});
 
 app.listen(port, () => {
-    console.log(`listening ${port}`)
-})
+  console.log(`listening ${port}`);
+});
 
 //I am going to add/register two tenants.
 //Now I am goint to add customer for each tenant.
